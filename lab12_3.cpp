3eaa9c30b3dcec2cf335769bd58523c37776ee2f
GARLIL25 <36127@dara.ac.th>
2026-01-05 21:14:49 +0700

    lab12_3.cpp

--- lab12_3.cpp
+++ lab12_3.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
-
 using namespace std;
 
-void mySwap(int &,int &); 
+int gcd(int x, int y);
 
 int main(){
-	int x,y;
-	cin >> x >> y;
-	cout << "Before swapping:\n";
-	cout << "x = " << x << ", y = " << y << "\n";
-	mySwap(x,y);
-	cout << "After swapping:\n";
-	cout << "x = " << x << ", y = " << y << "\n";
-	
-	return 0;
+    int x, y;
+    cin >> x >> y;
+    cout << gcd(x,y);
+    return 0; 
+}
+
+int gcd(int x, int y){
+    if(x % y == 0)
+        return y;
+    return gcd(y, x % y);
 }
