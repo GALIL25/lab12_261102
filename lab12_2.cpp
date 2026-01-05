35d2c7fd6d52ec7748537f88ed6c5b931b531db7
GARLIL25 <36127@dara.ac.th>
2026-01-05 21:09:51 +0700

    lab12_2.cpp

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,21 @@
-#include<iostream>
+#include <iostream>
+#include <ctime>
 using namespace std;
 
-int main(){
-    int x, y;
-    cin >> x >> y;
-    cout << gcd(x,y);
-    return 0; 
+long long int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    clock_t start = clock();
+
+    cout << "Result: " << fibonacci(50) << "\n";
+
+    clock_t end = clock();
+    double elapsed = double(end - start) / CLOCKS_PER_SEC;
+
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
