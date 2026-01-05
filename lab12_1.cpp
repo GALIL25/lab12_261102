81b4210bcab9139ad78d7e00ffa193bf160e2dfa
GARLIL25 <36127@dara.ac.th>
2026-01-05 21:23:53 +0700

    lab12_1.cpp

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,21 +1,21 @@
-#include<iostream>
+#include <iostream>
+#include <ctime>
 using namespace std;
 
-int fibonacci(int x);
-
-int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+long long int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-int fibonacci(int x)
-{
-    if (x == 0)
-        return 0;
-    else if (x == 1)
-        return 1;
-    else
-        return fibonacci(x - 1) + fibonacci(x - 2);
+int main() {
+    clock_t start = clock();
+
+    cout << "Result: " << fibonacci(50) << "\n";
+
+    clock_t end = clock();
+    double elapsed = double(end - start) / CLOCKS_PER_SEC;
+
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
